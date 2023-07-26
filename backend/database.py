0286fe8a745fd8742bbce89332dae40a7b6e8591
InchaeNa <dlsco1230@gmail.com>
2023-07-26 15:36:12 +0900

    first commit

---
+++ backend/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/gpt"
+
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')  # <- 수정
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
