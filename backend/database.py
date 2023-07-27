c0e068d759500f6fb0e7d08d40a1954f561ab144
InchaeNa <dlsco1230@gmail.com>
2023-07-27 17:12:46 +0900

    fix

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base  # 오타 수정
 from sqlalchemy.orm import sessionmaker
 import os
+
 DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/gpt"
 
-SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL', DATABASE_URL)  # 환경변수에 없을 경우 기본값 사용
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+Base = declarative_base()  # 여기에서 declarative_base()는 SQLAlchemy의 함수로서, 모든 모델 클래스의 슈퍼클래스를 생성합니다.
