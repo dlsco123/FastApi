b69b04cbd01c8a802c493328352a9f92857095fc
InchaeNa <dlsco1230@gmail.com>
2023-07-27 16:23:05 +0900

    "2nd"

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/gpt"
 
-SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')  # <- 수정
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
